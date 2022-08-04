06273ef2c5427a581cf313a5f72a0551a347fcc1
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-04 19:10:47 +0300

    feat(Todos): add a data model

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,18 +1,20 @@
-import React, {FC} from 'react'
+import React, { FC } from "react";
+import { Todo } from "../models/todo";
+
 
 type TodosProps = {
   children?: React.ReactNode;
-  items: string[];
-}
+  items: Todo[];
+};
 
 const Todos: FC<TodosProps> = (props) => {
   return (
     <ul>
-      {props.items.map(item => (
-        <li key={item}>{item}</li>
+      {props.items.map((item) => (
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
-  )
-}
+  );
+};
 
-export default Todos
+export default Todos;
