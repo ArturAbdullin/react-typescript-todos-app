d7b2d62dde8b84dbaeb9a3bfe5b84f20e143b324
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-04 19:24:58 +0300

    feat(Todos): add TodoItem component

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { Todo } from "../models/todo";
-
+import TodoItem from "./TodoItem";
 
 type TodosProps = {
   children?: React.ReactNode;
@@ -11,7 +11,7 @@ const Todos: FC<TodosProps> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
